6a5ead53998ef842dfc05074075694804af1998a
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-07-24 19:02:04 +0530

    Change the approach from using arrays to strings
    Finish the logic writing for all cases of inputs.

--- calculations.js
+++ calculations.js
@@ -1,48 +1,55 @@
-function add(num1,num2){
-    return num1 + num2
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(num1,num2){
-    return num1 - num2
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(num1,num2){
-    return num1 * num2
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(num1,num2){
-    return num1 / num2
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-let num1, operator, num2;
+let operator,
+  flag = 0;
 
-function operate(operator,num1,num2){
-    if (operator === '+')
-        return add(num1,num2);
+function operate(operator, num1, num2) {
+  if (operator === "+") return add(num1, num2);
 
-    if (operator === '-')
-        return subtract(num1,num2);
+  if (operator === "-") return subtract(num1, num2);
 
-    if (operator === '*')
-        return multiply(num1,num2);
+  if (operator === "*") return multiply(num1, num2);
 
-    if (operator === '/')
-        return divide(num1,num2);
+  if (operator === "/") return divide(num1, num2);
 }
 
-let disp_content = [];
-const displaydiv = document.querySelector('#display');
-const buttons = document.querySelectorAll('button');
+const displaydiv = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+let num1 = (num2 = "");
 
-buttons.forEach((btn)=>{
-    btn.addEventListener('click', ()=>{
-        disp_content.push(btn.value);
-        console.log(disp_content);
-        const show_disp = disp_content.reduce((acc,iter)=>{
-            return acc.toString() + iter.toString();
-            
-        })
-        displaydiv.textContent = show_disp;
-    })
-})
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.value === "AC") displaydiv.textContent = 0;
 
+    if (
+      btn.value === "+" ||
+      btn.value === "-" ||
+      btn.value === "*" ||
+      btn.value === "/"
+    ) {
+      flag = 1;
+      displaydiv.textContent = "";
+      operator = btn.value;
+    }
+    if (btn.value === "=") operate(operator, +num1, +num2);
+    else {
+      if (flag === 0) num1.concat(btn.value);
+
+      if (flag === 1) num2.concat(btn.value);
+    }
+  });
+});
