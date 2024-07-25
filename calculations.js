5dc8c86ea328a57ab8041c206da15a7733c3c7da
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-07-25 16:01:14 +0530

    Fix the issue where clicking the = button before selecting the second number was buggy.

--- calculations.js
+++ calculations.js
@@ -21,8 +21,12 @@ function operate(operator, num1, num2) {
   if (operator === "+") result = add(num1, num2);
   else if (operator === "-") result = subtract(num1, num2);
   else if (operator === "*") result = multiply(num1, num2);
-  else if (operator === "/") result = divide(num1, num2);
-  operator = '';
+  else if (operator === "/") {
+    result = divide(num1, num2);
+    console.log(result);
+    if (result === Infinity) result = "LOL NO";
+  }
+  operator = "";
 }
 
 const displaydiv = document.querySelector("#display");
@@ -31,7 +35,6 @@ let num1 = (num2 = "");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    console.log(btn.value);
     if (btn.value === "AC") {
       displaydiv.textContent = "0";
       num1 = "";
@@ -39,45 +42,44 @@ buttons.forEach((btn) => {
       flag = 0;
       operator = "";
       return;
-    }
-
-    else if (
+    } else if (
       btn.value === "+" ||
       btn.value === "-" ||
       btn.value === "*" ||
       btn.value === "/"
     ) {
-      if(flag===1){
+      if (flag === 1) {
         operate(operator, +num1, +num2);
         operator = btn.value;
         displaydiv.textContent = result;
         num1 = String(result);
-        num2 = '';
+        num2 = "";
+        return;
+      } else {
+        flag = 1;
+        displaydiv.textContent = num1;
+        operator = btn.value;
         return;
       }
-      //if flag is already 1 then call operate()
-      else{
-      flag = 1;
-      displaydiv.textContent = "0";
-      operator = btn.value;
-      return;
+    } else if (btn.value === "=") {
+      if (num2 === "") {
+        displaydiv.textContent = "0";
+        num1 = "";
+        flag = 0;
+        operator = "";
+        return;
+      } else {
+        operate(operator, +num1, +num2);
+        displaydiv.textContent = result;
+        num1 = String(result);
+        num2 = "";
+        flag = 0;
+        return;
       }
-    }
-    else if (btn.value === "=") {
-      operate(operator, +num1, +num2);
-      displaydiv.textContent = result;
-      num1 = String(result);
-      num2 = "";
-      flag = 0;
-      return;
-    }
-
-    else if (flag === 0) {
+    } else if (flag === 0) {
       num1 = num1.concat(btn.value);
       displaydiv.textContent = num1;
-    }
-
-    else if (flag === 1) {
+    } else if (flag === 1) {
       num2 = num2.concat(btn.value);
       displaydiv.textContent = num2;
     }
